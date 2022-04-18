ac8a01caf14d84bd276a9a6f541c768c3b5ca95f
Morten Fredriksen <morfre@equinor.com>
2022-04-18 11:03:08 +0200

    Add SDL event to keep window open

--- src/HelloSDL.cpp
+++ src/HelloSDL.cpp
@@ -43,8 +43,21 @@ int OpenWindow()
             //Update the surface
             SDL_UpdateWindowSurface( window );
 
-            //Wait two seconds
-            SDL_Delay( 10000 );
+            SDL_Event e;
+            bool quit = false;
+            while (!quit){
+                while (SDL_PollEvent(&e)){
+                    if (e.type == SDL_QUIT){
+                        quit = true;
+                    }
+                    if (e.type == SDL_KEYDOWN){
+                        quit = true;
+                    }
+                    if (e.type == SDL_MOUSEBUTTONDOWN){
+                        quit = true;
+                    }
+                }
+            }
         }
     }
         //Destroy window
